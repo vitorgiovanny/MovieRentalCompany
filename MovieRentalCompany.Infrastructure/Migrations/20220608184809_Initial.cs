591cd72eba4d78e7609a517066cf46f8b57f0c1e
Vitor Giovanny <vitor.dias@inventione.com.br>
2022-06-08 15:52:22 -0300

    Generation Migrations

---
+++ MovieRentalCompany.Infrastructure/Migrations/20220608184809_Initial.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieRentalCompany.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MovieRental",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Id_Customer = table.Column<int>(type: "int", nullable: false),
+                    Id_Movie = table.Column<int>(type: "int", nullable: false),
+                    Creater = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Devolution = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    PrevisionDevolution = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Canceled = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieRental", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MovieRental_Customer_Id_Customer",
+                        column: x => x.Id_Customer,
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MovieRental_Movie_Id_Movie",
+                        column: x => x.Id_Movie,
+                        principalTable: "Movie",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customer_Id",
+                table: "Customer",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_Id",
+                table: "Movie",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieRental_Id",
+                table: "MovieRental",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieRental_Id_Customer",
+                table: "MovieRental",
+                column: "Id_Customer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieRental_Id_Movie",
+                table: "MovieRental",
+                column: "Id_Movie");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovieRental");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
