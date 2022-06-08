96f8d01b62d7a0b3ae04a470d5a2afb54b894a1c
Vitor Giovanny <vitor.dias@inventione.com.br>
2022-06-08 17:05:03 -0300

    Controllers Entity

---
+++ MovieRentalCompany/Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieRentalCompany.Domain.Interfaces.Services;
+using MovieRentalCompany.ViewModel;
+
+namespace MovieRentalCompany.Controllers
+{
+    [ApiController]
+    [Route("Movie")]
+    public class MovieController : ControllerBase
+    {
+
+        private readonly IMovieServices _services;
+
+        public MovieController(IMovieServices services)
+        {
+            _services = services;
+        }
+
+        [HttpPost]
+        public IActionResult RegisterMovie(NewMovie model)
+        {
+            var movie = _services.RegisterMovie(model.Name, model.Category);
+
+            return Ok(movie);
+        }
+    }
+}
