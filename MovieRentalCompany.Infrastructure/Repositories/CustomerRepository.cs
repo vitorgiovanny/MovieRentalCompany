ef209d91c40d884222464b3de5befadf74a75832
Vitor Giovanny <vitor.dias@inventione.com.br>
2022-06-08 17:06:30 -0300

    Repositories Entities

---
+++ MovieRentalCompany.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,36 @@
+﻿using MovieRentalCompany.Domain.Entities;
+using MovieRentalCompany.Domain.Interfaces.Repositories;
+using MovieRentalCompany.Infrastructure.Database.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalCompany.Infrastructure.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Customer> Register(string name, string lastname, string email)
+        {
+            var customer = new Customer
+            {
+                Name = name,
+                LastName = lastname,
+                Email = email,
+                IsActive = true,
+            };
+
+            await _context.AddAsync(customer);
+            await _context.SaveChangesAsync();
+
+            return customer;
+        }
+    }
+}
---
+++ MovieRentalCompany.Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,43 @@
+﻿using MovieRentalCompany.Domain.Entities;
+using MovieRentalCompany.Domain.Interfaces.Repositories;
+using MovieRentalCompany.Infrastructure.Database.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalCompany.Infrastructure.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovieRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Movie> RegisterMovie(string name, string category)
+        {
+            var movie = new Movie
+            {
+                Name = name,
+                Category = category,
+                IsActive = true
+            };
+
+            try
+            {
+                await _context.Movie.AddAsync(movie);
+                await _context.SaveChangesAsync();
+            }
+            catch(Exception ex)
+            {
+                var message = ex;
+            }
+
+            return movie;
+        }
+    }
+}
