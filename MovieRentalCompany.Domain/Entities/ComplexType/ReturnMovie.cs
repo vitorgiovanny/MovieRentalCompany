f79edab1ba32f3f0175f4581da2453f28fe0c78a
Vitor Giovanny <vitor.dias@inventione.com.br>
2022-06-08 22:47:57 -0300

    Add Complext Type

---
+++ MovieRentalCompany.Domain/Entities/ComplexType/ReturnMovie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalCompany.Domain.Entities.ComplexType
+{
+    public class ReturnMovie
+    {
+        public DateTime PrevisionDevolution { get; set; }
+
+        public ReturnMovie(DateTime date)
+        {
+            PrevisionDevolution = date.AddDays(3);
+        }
+    }
+}
