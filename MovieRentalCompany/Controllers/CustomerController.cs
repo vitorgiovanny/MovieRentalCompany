5f20bc6f765b4f6d1583cd4dfafe58bb14af9ec8
Vitor Giovanny <vitor.dias@inventione.com.br>
2022-06-08 22:39:33 -0300

    Generation Controllers

---
+++ MovieRentalCompany/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieRentalCompany.Domain.Interfaces.Services;
+using MovieRentalCompany.ViewModel;
+
+namespace MovieRentalCompany.Controllers
+{
+    [ApiController]
+    [Route("Customer")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerServices _services;
+
+        public CustomerController(ICustomerServices services)
+        {
+            _services = services;
+        }
+
+        [HttpPost]
+        public IActionResult RegisterCustomer(NewCustomer customer)
+        {
+            var searchEmail = _services.GetByEmail(customer.Email);
+
+            if(searchEmail == null)
+            {
+                return Ok(_services.Register(customer.Name, customer.LasName, customer.Email));
+            }
+
+            return null;
+        }
+    }
+}
---
+++ MovieRentalCompany/Controllers/MovieRentalController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieRentalCompany.Domain.Interfaces.Services;
+using MovieRentalCompany.ViewModel;
+
+namespace MovieRentalCompany.Controllers
+{
+    [ApiController]
+    [Route("RentMovie")]
+    public class MovieRentalController : ControllerBase
+    {
+
+        private readonly IMovieRentalServices _services;
+
+        public MovieRentalController(IMovieRentalServices services)
+        {
+            _services = services;
+        }
+
+        [HttpPost]
+        public IActionResult Rental(int idCustom, int idMovie)
+        {
+            var rental = _services.Register(idCustom, idMovie);
+
+            var response = new MovieRentalViewModel
+            {
+                ReturnMovie = rental.PrevisionDevolution
+            };
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("devolution")]
+        public IActionResult Devolution(int id)
+        {
+            _services.Update(id);
+
+            return Ok();
+        }
+    }
+}
