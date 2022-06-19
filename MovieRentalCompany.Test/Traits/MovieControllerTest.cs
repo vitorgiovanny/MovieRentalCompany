80144b4454662e7222f6c2f2e4822e21b19f0428
Vitor Giovanny <vitor.dias@inventione.com.br>
2022-06-19 20:17:24 -0300

    Tests: Refactore and organizing test with Traits

--- MovieRentalCompany.Test/Traits/MovieControllerTest.cs
+++ MovieRentalCompany.Test/Traits/MovieControllerTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieRentalCompany.Controllers;
 using MovieRentalCompany.Domain.Interfaces.Services;
+using MovieRentalCompany.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,13 +23,32 @@ namespace MovieRentalCompany.Test
         }
 
         [Fact]
+        [Trait("Movie","Remover Filme pelo Id")]
         public void Remove_Movie_ReturnsOkResult()
         {
+            //Arrange
+            int idMovie = 1;
+
             // Act
-            var okResult = _controller.Remover(1);
+            var okResult = _controller.Remover(idMovie);
 
             // Assert
             Assert.IsType<OkObjectResult>(okResult);
+        }
+
+        [Fact]
+        [Trait("Movie", "Adicionar novo Filme")]
+        public void Create_Movie()
+        {
+            var movieNew = new NewMovie
+            {
+                Name = "Novo filmes teste",
+                Category = "Testes"
+            };
+
+            var result = _controller.RegisterMovie(movieNew);
+
+            Assert.IsType<OkObjectResult>(result);
         }
 
     }
